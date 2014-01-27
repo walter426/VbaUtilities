2758345b13f4936e206f71c30f91552d39a2164b
walter426 <waltertech426@gmail.com>
2014-01-27 17:12:24 +0800

    1.4.1

--- SQLiteCmdParser.py
+++ SQLiteCmdParser.py
@@ -14,10 +14,15 @@ def SQLiteCmdParser(Db_path, CmdFile_path):
     c = conn.cursor()
 
     cmd_file = open(CmdFile_path, 'r')
+    cmd = ""
     
     for line in cmd_file:
-        line = line.strip("\n")
-        c.execute(line)
+        line = line.strip("\n").strip(" ")
+        cmd = cmd + " " + line
+        
+        if cmd[-1] == ";":
+            c.execute(cmd)
+            cmd = ""
         
 
     conn.commit()
